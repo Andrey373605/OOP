81ca912187af0d2c8125a373b089c495a682deb9
Andrey373605 <martinovich2005@gmail.com>
2025-02-23 16:49:39 +0300

    added DataBaseHelper and DataBaseInitializer

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Data/DataBaseInitializer.cs
@@ -0,0 +1,38 @@
+﻿namespace OOP_LAB1.Infrastructure.Data;
+
+using Microsoft.Data.Sqlite;
+
+public class DatabaseInitializer
+{
+    private readonly string _connectionString;
+
+    public DatabaseInitializer(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void Initialize()
+    {
+        var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+        
+        CreateBanksTable(connection);
+
+    }
+
+    private void CreateBanksTable(SqliteConnection connection)
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS Banks 
+                (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL UNIQUE
+                );
+                    ";
+
+        command.ExecuteNonQuery();
+    }
+
+    
+}
--- LAB1/OOP_LAB1/OOP_LAB1/Program.cs
+++ LAB1/OOP_LAB1/OOP_LAB1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP_LAB1.Infrastructure.Data;
+
+DatabaseInitializer ini = new DatabaseInitializer("Data Source=sample.db");
+DatabaseHelper helper = new DatabaseHelper("Data Source=sample.db");
+var query = "INSERT INTO Banks (id, name) VALUES (@id, @name)";
+var parameters = new Dictionary<string, object>
+{
+    { "id", 1 },
+    { "name", "Example Bank" }
+};
+helper.ExecuteNonQuery(query, parameters);
+ini.Initialize();
