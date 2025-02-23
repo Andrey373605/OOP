69e61f392e16eb971c7f111c2bb858f9a86f2526
Andrey373605 <martinovich2005@gmail.com>
2025-02-23 16:53:12 +0300

    added DataBaseHelper

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Data/DataBaseHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace OOP_LAB1.Infrastructure.Data;
+
+public class DatabaseHelper
+{
+    private readonly string _connectionString;
+
+    public DatabaseHelper(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public void ExecuteNonQuery(string query, Dictionary<string, object> parameters = null)
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        using var command = new SqliteCommand(query, connection);
+
+        if (parameters != null)
+        {
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue($"@{param.Key}", param.Value ?? DBNull.Value);
+            }
+        }
+
+        command.ExecuteNonQuery();
+    }
+    
+    public List<Dictionary<string, object>> ExecuteQuery(string query, Dictionary<string, object> parameters = null)
+    {
+        var result = new List<Dictionary<string, object>>();
+
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        using var command = new SqliteCommand(query, connection);
+
+        if (parameters != null)
+        {
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue($"@{param.Key}", param.Value ?? DBNull.Value);
+            }
+        }
+
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var row = new Dictionary<string, object>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                row[reader.GetName(i)] = reader.IsDBNull(i) ? null : reader[i];
+            }
+            result.Add(row);
+        }
+
+        return result;
+    }
+    
+    public int GetLastInsertId()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        using var command = new SqliteCommand("SELECT last_insert_rowid();", connection);
+        var result = command.ExecuteScalar();
+
+        return Convert.ToInt32(result);
+    }
+    
+}
