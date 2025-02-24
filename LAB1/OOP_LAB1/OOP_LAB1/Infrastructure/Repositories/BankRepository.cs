a6d464747182ee068d6f7c2ce15377683d0a0a35
Andrey373605 <martinovich2005@gmail.com>
2025-02-24 12:07:34 +0300

    added BankRepository methods

--- LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Repositories/BankRepository.cs
+++ LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Repositories/BankRepository.cs
@@ -1,19 +1,83 @@
 ﻿using OOP_LAB1.Application.Interfaces;
 using OOP_LAB1.Domain.Entities;
+using OOP_LAB1.Infrastructure.Data;
 
 namespace OOP_LAB1.Infrastructure.Repositories;
 
 public class BankRepository : IBankRepository
 {
+    private readonly DatabaseHelper _databaseHelper;
+
+    public BankRepository(string connectionString)
+    {
+        _databaseHelper = new DatabaseHelper(connectionString);
+    }
     public void Add(Bank bank)
     {
-        throw new NotImplementedException();
+        var query = @"
+            INSERT INTO Banks (Name, IsEnterprise, EnterpriseType, LegalName, UNP, BIK, Address)
+            VALUES (@Name, @IsEnterprise, @EnterpriseType, @LegalName, @UNP, @BIK, @Address)";
+
+        var parameters = new Dictionary<string, object>
+        {
+            { "@Name", bank.Name },
+            { "@IsEnterprise", bank.IsEnterprise },
+            { "@EnterpriseType", bank.EnterpriseType },
+            { "@LegalName", bank.LegalName },
+            { "UNP", bank.UNP },
+            { "@BIK", bank.BIK },
+            { "@Address", bank.Address }
+        };
+        
+        _databaseHelper.ExecuteNonQuery(query, parameters);
+        bank.Id = _databaseHelper.GetLastInsertId();
     }
 
     public Bank GetById(int id)
     {
+        var query = @"
+                SELECT * 
+                FROM Bank WHERE     
+                Id = @Id";
+
+        var parameters = new Dictionary<string, object>
+        {
+            { "Id", id }
+        };
+        
+        var dataTable = _databaseHelper.ExecuteQuery(query, parameters);
+
+        if (dataTable.Count == 0)
+        {
+            return null!;
+        }
+        
+        var row = dataTable[0];
+        return new Bank
+        {
+            Id = Convert.ToInt32(row["Id"]),
+            Name = row["Name"].ToString(),
+            Address = row["Address"].ToString(),
+            IsEnterprise = Convert.ToBoolean(row["IsEnterprise"]),
+            EnterpriseType = row["Enterprise"]?.ToString(),
+            LegalName = row["LegalName"]?.ToString(),
+            UNP = row["UNP"]?.ToString(),
+            BIK = row["BIK"]?.ToString(),
+            UsersIdList = GetUsersIdListForBank(id),
+            EnterprisesIdList = GetEnterprisesIdListForBank(id)
+        };
+    }
+
+    private List<int> GetEnterprisesIdListForBank(int id)
+    {
         throw new NotImplementedException();
     }
+
+    private List<int> GetUsersIdListForBank(int id)
+    {
+        throw new NotImplementedException();
+    }
+
 
     public void Update(Bank bank)
     {
