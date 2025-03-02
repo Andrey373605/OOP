e7681de91b9c09fb10bb3840b0cf4b1591173e9c
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 21:00:33 +0300

    added password validator

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Validators/PasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿using OOP_LAB1.Presentation.ValidatorInterfaces;
+
+namespace OOP_LAB1.Presentation.Validators;
+
+public class PasswordValidator : IStringValidator
+{
+    public bool IsValid(string stringToValidate)
+    {
+        return true;
+    }
+
+    public string GetInvalidValidationString()
+    {
+        return "Password is too easy";
+    }
+}
