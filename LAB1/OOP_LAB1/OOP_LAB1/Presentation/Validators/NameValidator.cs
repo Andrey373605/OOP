fb86dc7d4eabee320c43d3543bea0080a3047c9c
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 21:00:45 +0300

    added name validator

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Validators/NameValidator.cs
@@ -0,0 +1,16 @@
+﻿using OOP_LAB1.Presentation.ValidatorInterfaces;
+
+namespace OOP_LAB1.Presentation.Validators;
+
+public class NameValidator : IStringValidator
+{
+    public bool IsValid(string stringToValidate)
+    {
+        return true;
+    }
+
+    public string GetInvalidValidationString()
+    {
+        return "Name is invalid";
+    }
+}
