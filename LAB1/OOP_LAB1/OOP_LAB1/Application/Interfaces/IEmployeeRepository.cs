589d0f8fc6d89585a17577b03ab04639e8affffc
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 15:13:33 +0300

    added emaployee repository

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using OOP_LAB1.Domain.Entities;
+namespace OOP_LAB1.Application.Interfaces;
+
+public interface IEmployeeRepository
+{
+    public Task<Employee> GetByEmailAsync(string email);
+
+    public Task AddAsync(Employee employee);
+}
---
+++ LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using OOP_LAB1.Application.Interfaces;
+using OOP_LAB1.Domain.Entities;
+
+namespace OOP_LAB1.Infrastructure.Repositories;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    public Task<Employee> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+}
