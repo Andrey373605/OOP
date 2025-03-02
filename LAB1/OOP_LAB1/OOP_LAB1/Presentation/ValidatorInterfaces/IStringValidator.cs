fd76c2f4ddffa8ca9f41fce354526c4acd79abae
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 21:00:11 +0300

    added interface validator

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/ValidatorInterfaces/IStringValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace OOP_LAB1.Presentation.ValidatorInterfaces;
+
+public interface IStringValidator
+{
+    public bool IsValid(string stringToValidate);
+    public string GetInvalidValidationString();
+}
