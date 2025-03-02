c9638e6a01e6271e7674854c1394fbc6961c220f
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 20:59:05 +0300

    added console view

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Console/ConsoleView.cs
@@ -0,0 +1,9 @@
+﻿namespace OOP_LAB1.Presentation.Console;
+
+public class ConsoleView : IConsoleView
+{
+    public void WriteLine(string message) => System.Console.WriteLine(message);
+    public string ReadLine() => System.Console.ReadLine();
+    public void Clear() => System.Console.Clear();
+
+}
---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Console/IConsoleView.cs
@@ -0,0 +1,8 @@
+﻿namespace OOP_LAB1.Presentation.Console;
+
+public interface IConsoleView
+{
+    void WriteLine(string message);
+    string ReadLine();
+    void Clear();
+}
