4868781e1ceb16da90b34bf06c050c040e6581db
Andrey3736 <martinovich2005@gmail.com>
2025-03-02 15:12:34 +0300

    added user and employee context

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Context/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using OOP_LAB1.Domain.Entities;
+namespace OOP_LAB1.Application.Context;
+
+
+public class UserContext
+{
+    public User CurrentUser { get; private set; }
+
+    public void SetCurrent(User user)
+    {
+        CurrentUser = user;
+    }
+
+    public void ClearCurrent()
+    {
+        CurrentUser = null;
+    }
+}
+
+
