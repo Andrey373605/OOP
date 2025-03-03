f70fb246d848cf10bc86cea2237e19640750514b
Andrey3736 <martinovich2005@gmail.com>
2025-03-03 12:48:05 +0300

    added registration view

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/RegistrationView.cs
@@ -0,0 +1,49 @@
+﻿using OOP_LAB1.Domain.Entities;
+using OOP_LAB1.Domain.Interfaces;
+using OOP_LAB1.Presentation.Console;
+using OOP_LAB1.Presentation.Enums;
+using OOP_LAB1.Presentation.Handler;
+using OOP_LAB1.Presentation.Validators;
+
+namespace OOP_LAB1.Presentation.Views;
+
+public class RegistrationView : IView
+{
+    private readonly IInputHandler _input;
+    private readonly IAuthorizationService _auth;
+    private readonly IConsoleView _console;
+    
+    public PageName? NextViewName { get; private set; }
+
+    public RegistrationView(IInputHandler input, IConsoleView console, IAuthorizationService authService)
+    {
+        _input = input;
+        _auth = authService;
+        _console = console;
+    }
+
+    public void Execute()
+    {
+        string firstName = _input.GetString("First name: ", new NameValidator());
+        string lastName = _input.GetString("Last name: ", new NameValidator());
+        string middleName = _input.GetString("Middle name: ", new NameValidator());
+        string email = _input.GetString("Email: ", new EmailValidator());
+        string phoneNumber = _input.GetString("Phone number: ", new PhoneValidator());
+        string series = _input.GetString("Series: ", new SeriesValidator());
+        string identificationNumber = _input.GetString("Identification number: ", new IdentificationNumberValidator());
+        string password = _input.GetString("Password: ", new PasswordValidator());
+        
+        try
+        {
+            _auth.RegisterUserAsync(firstName, lastName, middleName, email, password, phoneNumber,
+                identificationNumber, series).GetAwaiter().GetResult();
+            NextViewName = PageName.BankChoosePage;
+            _console.WriteLine("Registration successful!");
+        }
+        catch (Exception ex)
+        {
+            _console.WriteLine($"Error: {ex.Message}");
+        }
+        
+    }
+}
--- LAB1/OOP_LAB1/OOP_LAB1/Program.cs
+++ LAB1/OOP_LAB1/OOP_LAB1/Program.cs
@@ -5,13 +5,46 @@ using OOP_LAB1.Application.Services;
 using OOP_LAB1.Domain.Interfaces;
 using OOP_LAB1.Infrastructure.Repositories;
 using OOP_LAB1.Presentation;
+using OOP_LAB1.Presentation.Console;
 using OOP_LAB1.Presentation.Controllers;
+using OOP_LAB1.Presentation.Enums;
+using OOP_LAB1.Presentation.Handler;
+using OOP_LAB1.Presentation.Navigator;
+using OOP_LAB1.Presentation.Views;
 
-
+// Настройка DI
 var serviceProvider = new ServiceCollection()
+    // регистрация сервисов
     .AddSingleton<IAuthorizationService, AuthorizationService>()
+    .AddSingleton<IBankService, BankService>()
+    
+    // регистрация консоли 
+    .AddSingleton<IConsoleView, ConsoleView>()
+    
+    // регистрация handler
+    .AddSingleton<IInputHandler, InputHandler>()
+    
+    // регистрация navigator
+    .AddSingleton<INavigator, Navigator>()
+    
+    // регистрация репозиториев
     .AddSingleton<IUserRepository, UserRepository>()
     .AddSingleton<IEmployeeRepository, EmployeeRepository>()
+    
+    // регистрация view
+    .AddTransient<MainMenuView>()
+    .AddTransient<RegistrationView>()
+    .AddTransient<ChooseBankView>()
+    
+    // сборка
     .BuildServiceProvider();
 
 
+var navigator = serviceProvider.GetService<INavigator>();
+
+navigator.RegisterView(PageName.BankChoosePage, serviceProvider.GetService<ChooseBankView>());
+navigator.RegisterView(PageName.MainMenuPage, serviceProvider.GetService<MainMenuView>());
+navigator.RegisterView(PageName.RegistrationPage, serviceProvider.GetService<RegistrationView>());
+
+
+navigator.Run(PageName.MainMenuPage);
