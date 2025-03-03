a68a1fe18d4bb28f5ee59b0c6416c77fd3396f4b
Andrey3736 <martinovich2005@gmail.com>
2025-03-03 12:47:35 +0300

    added main menu view

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/MainMenuView.cs
@@ -0,0 +1,28 @@
+﻿using OOP_LAB1.Presentation.Console;
+using OOP_LAB1.Presentation.Enums;
+
+namespace OOP_LAB1.Presentation.Views;
+
+public class MainMenuView : IView
+{
+    public PageName? NextViewName { get; private set; }
+    private readonly IConsoleView _console;
+
+    public MainMenuView(IConsoleView console)
+    {
+        _console = console;
+    }
+
+    public void Execute()
+    {
+        _console.WriteLine("1. Choose bank");
+        _console.WriteLine("2. Exit");
+        
+        var choice = _console.ReadLine();
+        NextViewName = choice switch
+        {
+            "1" => PageName.BankChoosePage,
+            _ => NextViewName
+        };
+    }
+}
