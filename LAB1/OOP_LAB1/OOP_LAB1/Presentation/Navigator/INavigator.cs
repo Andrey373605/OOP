37607cb3e9afaf070a69ea79aca78c8c26ea11a9
Andrey3736 <martinovich2005@gmail.com>
2025-03-03 12:47:24 +0300

    updated INavigator

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Navigator/INavigator.cs
@@ -0,0 +1,11 @@
+﻿using OOP_LAB1.Presentation.Views;
+using OOP_LAB1.Presentation.Enums;
+namespace OOP_LAB1.Presentation.Navigator;
+
+public interface INavigator
+{
+    public void RegisterView(PageName pageName, IView view);
+
+    public void Run(PageName? startViewName);
+
+}
