7ac7a2f3435977adf574346d6b600b266a72230c
Andrey3736 <martinovich2005@gmail.com>
2025-03-03 12:43:27 +0300

    added test functional to bankservice

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Services/BankService.cs
@@ -0,0 +1,22 @@
+﻿using OOP_LAB1.Domain.Entities;
+using OOP_LAB1.Domain.Interfaces;
+
+namespace OOP_LAB1.Application.Services;
+
+public class BankService : IBankService
+{
+    public void CreateBank()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetAllBankNames()
+    {
+        return new List<string>(["ZBank", "TBank", "VBank"]);
+    }
+
+    public Bank GetBankByName(string name)
+    {
+        return new Bank{ Name = name };
+    }
+}
