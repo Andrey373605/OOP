7ce39995b23ab37c5e0bfbb44ca2e921f8a7b8f8
Andrey3736 <martinovich2005@gmail.com>
2025-03-04 13:03:35 +0300

    updated ChooseBankView.cs

--- LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/ChooseBankView.cs
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/ChooseBankView.cs
@@ -1,44 +1,47 @@
-﻿using OOP_LAB1.Domain.Interfaces;
+﻿using OOP_LAB1.Application.Context;
+using OOP_LAB1.Application.Services;
+using OOP_LAB1.Domain.Entities;
+using OOP_LAB1.Domain.Interfaces;
 using OOP_LAB1.Presentation.Console;
 using OOP_LAB1.Presentation.Enums;
 using OOP_LAB1.Presentation.Handler;
 using OOP_LAB1.Presentation.Validators;
-using OOP_LAB1.Domain.Entities;
 
 namespace OOP_LAB1.Presentation.Views;
 
 public class ChooseBankView : IView
 {
-    private readonly IConsoleView _console;
-    private readonly IBankService _bankService;
-    private readonly IInputHandler _input;
-
+    public PageName? NextViewName { get; private set; }
     
-    public ChooseBankView(IConsoleView console, IInputHandler input, IBankService bankService)
+
+    private readonly IInputHandler _input;
+    private readonly IAuthorizationService _auth;
+    private readonly IConsoleView _console;
+    private readonly IContext _context;
+    private readonly IBankService _bankService;
+
+    public ChooseBankView(IInputHandler input, IAuthorizationService auth, IConsoleView console, IContext context, IBankService bankService)
     {
-        _console = console;
-        _bankService = bankService;
         _input = input;
+        _auth = auth;
+        _console = console;
+        _context = context;
+        _bankService = bankService;
     }
-
-    public PageName? NextViewName { get; set; }
-
+    
     public void Execute()
     {
-        _console.WriteLine("List of bank:");
-        List<string> bankNames = _bankService.GetAllBankNames().ToList();
-        foreach (var b in bankNames)
+        List<string> banks = _bankService.GetAllBankNames().ToList();
+        foreach (string b in banks)
         {
             _console.WriteLine(b);
         }
-        
-        string bankName = _input.GetString("Enter bank name: ", new NameValidator());
-        
-        Bank bank = _bankService.GetBankByName(bankName);
-        
-        _console.WriteLine($"Successfully chosen bank: {bank.Name}");
+        var bankName = _input.GetString("Enter bank name: ", new NameValidator());
+        var bank = _bankService.GetBankByName(bankName);
+        _bankService.LoginBank(bank);
+        _console.WriteLine($"succesfully chosen bank {bankName}");
+        _context.SetCurrent(bank);
 
-        NextViewName = PageName.BankMenuPage;
-        
+        NextViewName = PageName.ChooseRolePage;
     }
 }
