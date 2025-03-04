e05d9f6fa9d4c88c1bfdd362b707208ad40ce7ee
Andrey3736 <martinovich2005@gmail.com>
2025-03-04 13:03:45 +0300

    added Employee.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Domain/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿using OOP_LAB1.Domain.Enums;
+
+namespace OOP_LAB1.Domain.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public UserRole Role { get; set; }
+}
