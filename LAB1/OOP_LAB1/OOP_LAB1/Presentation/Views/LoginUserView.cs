cead2c015e5e8a715fb5f45a99b81bdfbb76667e
Andrey3736 <martinovich2005@gmail.com>
2025-03-04 13:03:57 +0300

    added LoginUserView.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/LoginUserView.cs
@@ -0,0 +1,43 @@
+﻿using OOP_LAB1.Application.Context;
+using OOP_LAB1.Domain.Interfaces;
+using OOP_LAB1.Presentation.Console;
+using OOP_LAB1.Presentation.Enums;
+using OOP_LAB1.Presentation.Handler;
+using OOP_LAB1.Presentation.Validators;
+
+namespace OOP_LAB1.Presentation.Views;
+
+public class LoginUserView : IView
+{
+    private readonly IInputHandler _input;
+    private readonly IAuthorizationService _auth;
+    private readonly IConsoleView _console;
+    private readonly IContext _context;
+
+    public LoginUserView(IInputHandler input, IAuthorizationService auth, IConsoleView console, IContext context)
+    {
+        _input = input;
+        _auth = auth;
+        _console = console;
+        _context = context;
+    }
+    public PageName? NextViewName { get; private set; }
+    public void Execute()
+    {
+        string email = _input.GetString("Email: ", new EmailValidator());
+        string password = _input.GetString("Password: ", new PasswordValidator());
+
+        try
+        {
+            _auth.AuthenticateUserAsync(email, password);
+            _console.WriteLine($"Successfully logged in");
+        }
+        catch (Exception e)
+        {
+            _console.WriteLine(e.Message);
+            NextViewName = PageName.MainMenuPage;
+        }
+        
+        NextViewName = PageName.ChooseBankPage;
+    }
+}
