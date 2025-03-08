ab96d1a8e166356525c63f8e6fb8387138749fdf
Andrey3736 <martinovich2005@gmail.com>
2025-03-08 17:39:22 +0300

    added IAccountEnterpriseRepository.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Interfaces/IAccountEnterpriseRepository.cs
@@ -0,0 +1,9 @@
+﻿using OOP_LAB1.Domain.Entities;
+
+namespace OOP_LAB1.Application.Interfaces;
+
+public interface IAccountEnterpriseRepository
+{
+    Task<Account> GetByIdAsync(int accountId);
+    Task UpdateAsync(Account projectAccount);
+}
