5d1df973992af156c290c2653a4995726de8f040
Andrey3736 <martinovich2005@gmail.com>
2025-03-08 10:50:41 +0300

    added IInstalmentRepository

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Interfaces/IInstallmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using OOP_LAB1.Domain.Entities;
+
+namespace OOP_LAB1.Application.Interfaces;
+
+public interface IInstallmentRepository
+{
+    public Task AddAsync(Installment installment);
+    
+    public Task CreateAsync(Installment installment);
+    
+    public Task UpdateAsync(Installment installment);
+    
+    public Task DeleteAsync(Installment installment);
+    public Task<Installment> GetByIdAsync(int installmentId);
+}
