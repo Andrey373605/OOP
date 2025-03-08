2792bad13f20036aa55f398d1d5424d3bc793ae3
Andrey3736 <martinovich2005@gmail.com>
2025-03-08 17:39:10 +0300

    added BankRepository.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Infrastructure/Repositories/BankRepository.cs
@@ -0,0 +1,32 @@
+﻿using OOP_LAB1.Application.Interfaces;
+using OOP_LAB1.Domain.Entities;
+
+namespace OOP_LAB1.Infrastructure.Repositories;
+
+public class BankRepository : IBankRepository
+{
+    public Task AddAsync(Bank bank)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Bank> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Bank> GetByNameAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Bank bank)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<string>> GetAllBankNamesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
