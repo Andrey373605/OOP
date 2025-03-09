a12c9c4afd64457a609fd8bd4e75f5f34952a007
Andrey3736 <martinovich2005@gmail.com>
2025-03-09 13:19:31 +0300

    added ExitView.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Presentation/Views/ExitView.cs
@@ -0,0 +1,19 @@
+﻿using OOP_LAB1.Presentation.Console;
+using OOP_LAB1.Presentation.Enums;
+
+namespace OOP_LAB1.Presentation.Views;
+
+public class ExitView : IView
+{
+    IConsole _console;
+
+    public ExitView(IConsole console)
+    {
+        _console = console;
+    }
+    public PageName? NextViewName { get; }
+    public void Execute()
+    {
+        _console.WriteLine("Exit");
+    }
+}
