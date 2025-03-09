4f4eb2fa0a7d3fc32c91bb94a0096369eff793f7
Andrey3736 <martinovich2005@gmail.com>
2025-03-09 11:03:13 +0300

    added IEmployeeRepository.cs

---
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using OOP_LAB1.Domain.Entities;
+
+namespace OOP_LAB1.Application.Interfaces;
+
+public interface IEmployeeRepository
+{
+    public Task AddAsync(Employee employee);
+    public Task DeleteAsync(Employee employee);
+    public Task<Employee> GetByIdAsync(int id);
+    public Task UpdateAsync(Employee employee);
+}
