856931ae02c32a9a711db82df2bd12b7efcc10bf
Andrey3736 <martinovich2005@gmail.com>
2025-03-09 11:07:18 +0300

    updated AccountService.cs

--- LAB1/OOP_LAB1/OOP_LAB1/Application/Services/AccountService.cs
+++ LAB1/OOP_LAB1/OOP_LAB1/Application/Services/AccountService.cs
@@ -21,6 +21,11 @@ public class AccountService : IAccountService
     public async Task FreezeAccountAsync(int id)
     {
         Account account = await _accountRepository.GetByIdAsync(id);
+        if (account == null)
+        {
+            throw new NullReferenceException("Account not found");
+        }
+        
         account.FreezeAccount();
         await _accountRepository.UpdateAsync(account);
         
@@ -29,6 +34,11 @@ public class AccountService : IAccountService
     public async Task UnfreezeAccountAsync(int id)
     {
         Account account = await _accountRepository.GetByIdAsync(id);
+        if (account == null)
+        {
+            throw new NullReferenceException("Account not found");
+        }
+        
         account.UnfreezeAccount();
         await _accountRepository.UpdateAsync(account);
     }
@@ -36,6 +46,11 @@ public class AccountService : IAccountService
     public async Task BlockAccountAsync(int id)
     {
         Account account = await _accountRepository.GetByIdAsync(id);
+        if (account == null)
+        {
+            throw new NullReferenceException("Account not found");
+        }
+        
         account.BlockAccount();
         await _accountRepository.UpdateAsync(account);
     }
@@ -43,6 +58,11 @@ public class AccountService : IAccountService
     public async Task UnblockAccountAsync(int id)
     {
         Account account = await _accountRepository.GetByIdAsync(id);
+        if (account == null)
+        {
+            throw new NullReferenceException("Account not found");
+        }
+        
         account.UnblockAccount();
         await _accountRepository.UpdateAsync(account);
     }
@@ -51,10 +71,9 @@ public class AccountService : IAccountService
     public async Task DeleteAccountAsync(int id)
     {
         Account account = await _accountRepository.GetByIdAsync(id);
-
         if (account == null)
         {
-            throw new NullReferenceException();
+            throw new NullReferenceException("Account not found");
         }
         
         await _accountRepository.RemoveAsync(account);
@@ -63,6 +82,11 @@ public class AccountService : IAccountService
     public async Task DepositAccountAsync(int accountId, decimal amount)
     {
         Account account = await _accountRepository.GetByIdAsync(accountId);
+        if (account == null)
+        {
+            throw new NullReferenceException("Account not found");
+        }
+        
         account.DepositAccount(amount);
         await _accountRepository.UpdateAsync(account);
     }
